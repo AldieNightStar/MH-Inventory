9ae6cff4184a6f0c54b12acef17f53e8eeffc2f5
AldieNightStar <AldieNightStar@gmail.com>
2024-09-16 01:49:09 +0200

    more doc

--- export/haxidenti/inv/Registry.ts
+++ export/haxidenti/inv/Registry.ts
@@ -1,5 +1,8 @@
 namespace haxidenti.inventory {
 
+	/**
+	 * Item information. Do not change it in runtime
+	 */
 	export class ItemInfo {
 		constructor(
 			public name: string,
@@ -8,14 +11,23 @@ namespace haxidenti.inventory {
 		) {}
 	}
 
+	/**
+	 * Default maximum stack
+	 */
 	export let DEFAULT_ITEM_STACK = 32;
 
 	const ITEM_REG: {[k: number]: ItemInfo} = {}
 
+	/**
+	 * Get item information by its id
+	 */
 	export function getRegistryItem(id: number): ItemInfo | undefined {
 		return ITEM_REG[id]
 	}
 
+	/**
+	 * Add new item. Returns its id
+	 */
 	export function registerItem(id: number, name: string, maxStack: number = 32, tags: string[] = []): number {
 		if (maxStack < 1) maxStack = 1;
 		if (id < 1) throw new Error("Id of item should be greater than zero");
