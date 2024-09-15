5f5580251068ebf6c0430fad94973085b5301221
AldieNightStar <AldieNightStar@gmail.com>
2024-09-16 01:17:35 +0200

    More readable

--- passages/Start.ts
+++ passages/Start.ts
@@ -1,16 +1,18 @@
 // This is your first passage
 Passage.of("Start", s => {
+	const Inventory = haxidenti.inventory.Inventory;
+	const registerItem = haxidenti.inventory.registerItem;
 
-	const BREAD = haxidenti.inventory.registerItem(1, "🥖Bread", 4);
-	const BRANCH = haxidenti.inventory.registerItem(2, "🌿Branch", 4);
-	const ROCK = haxidenti.inventory.registerItem(3, "🪨Rock", 16);
+	const BREAD = registerItem(1, "🥖Bread", 4);
+	const BRANCH = registerItem(2, "🌿Branch", 4);
+	const ROCK = registerItem(3, "🪨Rock", 16);
 
-	const inv1 = new haxidenti.inventory.Inventory(32);
+	const inv1 = new Inventory(32);
 	console.log("Add BREAD", inv1.addItem(BREAD, 7));
 	console.log("Add BRANCH", inv1.addItem(BRANCH, 32));
 	console.log("Add ROCK", inv1.addItem(ROCK, 32));
 	
-	const inv2 = new haxidenti.inventory.Inventory(32);
+	const inv2 = new Inventory(32);
 
 	s.title("Inv1")
 	s.print(inv1.ui(inv2));
